7f788c14f8f18e8a2cdabcc942db3a1cfc5756b7
Neil Dias <neildias.ai@gmail.com>
2021-06-08 01:08:06 +0530

    improved live tracker

--- old_routine_generator/live_practice_tracker.py
+++ old_routine_generator/live_practice_tracker.py
@@ -4,85 +4,42 @@
 import pandas as pd
 from datetime import date
 import time
+from utils import practice_headers, utilities, countdown_timer
 
 
-path = "P:\\"
-
-filename = "guitar_practice_tracker.xlsx"
+# hardcoded path
+path = practice_headers.path
+filename = practice_headers.filename
+topics = practice_headers.topics
 
 today = date.today()
-
-topics = {
-    "Music_Theory": 0,
-    "Sight Reading (Other non Graded Pieces)": 0,
-    "Ear Training": 0,
-    "Sight Playing (Graded Studies|Classical Pieces)": 0,
-    "Kitharologus": 0,
-    "Rhythm (Song) Practice": 0,
-    "Lead (Song) Pieces": 0,
-    "FretBoard Mastery": 0,
-    "Work on Scales": 0,
-    "Strum Chords": 0,
-    "Arpeggiate Chords": 0,
-    "Slurs": 0,
-    "Vibrato": 0,
-    "Slide": 0,
-    "Bends": 0,
-    "Picado": 0,
-    "Rasgeo": 0,
-    "Pulgar": 0,
-    "Alzapua": 0,
-    "Golpe": 0,
-    "Natural Harmonics": 0,
-    "Artificial Harmonics": 0,
-    # "Solea": 0,
-    # "Alegrias": 0,
-    # "Tangos": 0,
-    # "Seguiriya": 0,
-    # "Tarantas": 0,
-    # "Granainas": 0,
-    # "Bulerias": 0,
-    "Improvisation": 0,
-    # "Guitar Licks": 0,
-    # "Flamenco RH": 0,
-    # "Flamenco LH": 0,
-    "Tremelo": 0,
-    # "Sweep Picking": 0,
-    # "Hybrid Picking": 0,
-    # "Tapping": 0,
-    # "Harp Harmonics": 0,
-    # "Slap Harmonics:": 0,
-    # "Percussive": 0,
-    # "Whammy bar": 0,
-    # "Pedals": 0,
-  }
 
 lesson_1 = {
 
     # Classical
     # lesson 1 total 30 mins
-    "Spider Warmup": 2, #5,
-    "Ear Training": 2,  #5,
-    "Sight Playing (Graded Studies|Classical Pieces)": 2, #20,
+    "Spider Warmup": 5,
+    "Ear Training": 5,
+    "Sight Playing (Graded Studies|Classical Pieces)": 20,
 
 }
 
 lesson_2 = {
 
     # Scales
-    "Scales Pick": 1, #10,
-    "slurs": 1, #5,
-    "Vibrato": 1, #5,
-    "Picado": 1, #10,
+    "Scales Pick": 10,
+    "slurs": 5,
+    "Vibrato": 5,
+    "Picado": 10,
 }
 
 lesson_3 = {
 
     # technical workout
     # lesson 2 total 35 mins
-    "FretBoard Mastery": 1, #5,
-    "Kitharologus": 1, #20,
-    "Natural Harmonics": 1, #5,
+    "FretBoard Mastery": 5,
+    "Kitharologus": 20,
+    "Natural Harmonics": 5,
 }
 
 # add chord workout on even days of the week
@@ -91,10 +48,10 @@ add_chord_workout = "Strum Chords" if today.day % 2 == 0 else "Arpeggiate Chords
 lesson_4 = {
 
     # Chords
-    "Strum Chords": 1, #10,
+    "Strum Chords": 10,
     # strum on even days, else Arpeggiate
-    add_chord_workout: 1, #10,
-    "Rasgeo": 1, #10,
+    add_chord_workout: 10,
+    "Rasgeo": 10,
 
 }
 
@@ -105,11 +62,11 @@ lesson_5 = {
 
     # technical workout - others
     # Improvise on even days, else play rhythm songs
-    creative_workout: 1, #10,
-    "Bends": 1, #5,
-    "Alzapua": 1, #5,
-    "Tremelo": 1, #5,
-    "Chord Ear Training": 1, #5,
+    creative_workout: 10,
+    "Bends": 5,
+    "Alzapua": 5,
+    "Tremelo": 5,
+    "Chord Ear Training": 5,
 
 }
 
@@ -141,139 +98,6 @@ lessons_key = {
 }
 
 
-def get_file_path(path: str, filename: str, data_dict: dict):
-  """
-  Lets user change file path, filename and add more topic headers to the
-  practice routine
-
-  :param path: str
-  :param filename: str
-  :param data_dict: dict
-  :return: None if no change opted else returns filepath, filename,
-           modified_data_dict
-  """
-  # instantiate new variables
-  file_directory = file_name = ""
-
-  # change directory
-  change_directory = input(
-    f"Do you want to change the current file path: {path}, "
-    f"or the current file_name: '{filename}'.\n"
-    f"Or add to the current topics/heads of practice: ? "
-    f"Enter 'y' if you want to change that, else press ENTER. "
-  )
-  if change_directory == 'y':
-    invalid_directory = True
-    while invalid_directory:
-      file_directory = input('Type a new path, else press ENTER for no change ')
-      # if opted not to change
-      if not file_directory:
-        invalid_directory = False
-      else:  # if directory inputted test if its a valid file path
-        if os.path.exists(file_directory):
-          invalid_directory = False
-        else:
-          print("Invalid file directory. Enter a valid file directory")
-
-    # change filename
-    invalid_filename = True
-    while invalid_filename:
-      file_name = input("Type new filename, else press ENTER for no change ")
-      # if opted not to change
-      if not file_name:
-        invalid_filename = False
-      else:  # check if inputted filename is valid
-        if file_name.isalnum():
-          invalid_filename = False
-        else:
-          print("The file name is invalid. Enter again.")
-
-    # alter topic headers
-    print("The current heads of practice are as follows: ")
-    pprint([key for key in data_dict.keys()])
-    change_topic_headers = input(
-      "Press y to opt to change the topic headers, else type ENTER "
-    )
-    if change_topic_headers == "y":
-      new_topic_headers = input(
-        "Type the name of new topic heads separated by space, then enter : "
-      )
-      for topic_head in new_topic_headers.split():
-        # topic head sanity check
-        while not topic_head.isalpha():
-          topic_head = input(
-            f"Previous entry {topic_head} must be all alphabets. Try again: "
-          )
-        data_dict[topic_head] = 0
-    return file_directory, file_name, data_dict
-
-  else:   # No change
-    return None, None, None
-
-
-def read_create_database_object(path: str, filename: str, data_dict: dict):
-  """
-  Returns a dataframe based on existing files or by creating a new one
-
-  :param path: str
-  :param filename: str
-  :param data_dict: dict
-  :return: df
-  """
-  try:
-    # check if an excel file is available
-    df = pd.read_excel(path + filename, index_col=0)
-    print("Found existing database")
-
-  except FileNotFoundError:
-    # if unavailable create a dataframe, to be later exported as excel;
-    print("Existing database not found. Creating a new database (dataframe)\n")
-
-    df = pd.DataFrame(data_dict,
-                      index=pd.date_range(start="06-03-2021",
-                                          periods=1,
-                                          freq="D"))
-    df["Total Mins"] = 0  # last col
-
-  return df
-
-
-# get date for the index column of the dataframe for the new inputs
-def get_date():
-  """
-  Retrieve custom or current date in the appropriate format
-
-  :return: python or pandas date object. Later if user inputs a date
-  """
-
-  while True:
-    custom_date = input(
-      "To input a custom date, enter the same in dd-mm-yyyy;"
-      " else press ENTER : "
-    )
-    if not custom_date:
-      return today
-    else:
-      try:
-        return pd.to_datetime(custom_date, format="%d-%m-%Y")
-      except ValueError:
-        print("Wrong Input!!! Either press plain enter key, or provide date in "
-              "dd-mm-yyyy format. Try again!")
-
-
-def get_hours_minutes_seconds(time_in_seconds: int):
-  """
-  Converts seconds to hours, minute and seconds
-
-  :param time_in_seconds: int
-  :return: int | hours, minutes, seconds
-  """
-
-  minutes, seconds = divmod(time_in_seconds, 60)
-  hours, minutes = divmod(minutes, 60)
-  return hours, minutes, seconds
-
-
 def countdown(time_in_minutes: int, topic_header: str):
   """
   :param time_in_seconds: int
@@ -288,7 +112,7 @@ def countdown(time_in_minutes: int, topic_header: str):
 
   # print timer
   while timer > 0:
-    hours, minutes, seconds = get_hours_minutes_seconds(timer)
+    hours, minutes, seconds = countdown_timer.get_hours_minutes_seconds(timer)
     time_to_display = f"\r{str(hours).zfill(2)}:{str(minutes).zfill(2)}" \
                       f":{str(seconds).zfill(2)}"
     print(time_to_display, end="")
@@ -403,19 +227,19 @@ def column_reorder(df, col_name, position):
 
 if __name__ == '__main__':
     # get new_path, filename if the user chooses
-    new_path, new_filename, new_topic_dict = get_file_path(path,
-                                                           filename,
-                                                           topics)
+    new_path, new_filename, new_topic_dict = utilities.get_file_path(path,
+                                                                     filename,
+                                                                     topics)
 
     path = new_path if new_path else path
     filename = new_filename+".xlsx" if new_filename else filename
     topics = new_topic_dict if new_topic_dict else topics
 
     # get data
-    df = read_create_database_object(path, filename, topics)
+    df = utilities.read_create_database_object(path, filename, topics)
 
     # store date in the update_date variable
-    update_date = get_date()
+    update_date = utilities.get_date()
     print()
 
     # get a dictionary of key, value updates
@@ -429,7 +253,7 @@ if __name__ == '__main__':
 
     # ensure Total Mins is Last col
     position = len(df.columns) - 1
-    df = column_reorder(df, "Total Mins", position)
+    df = utilities.column_reorder(df, "Total Mins", position)
 
     print(df.T)
 
